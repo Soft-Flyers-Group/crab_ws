3ddcb623f24f8d2eac0218337ea0b6ec5757d4dc
odinroast <ruthvick2005@gmail.com>
2026-07-04 13:40:53 -0400

    Added code to be able to move a single servo with Dynamixel SDK and u2d2 with ros2 jazzy

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 How to clone:
 git clone https://github.com/Soft-Flyers-Group/crab_ws --recurse-submodules     
+
+Link to dynamixel docs for ros2:
+https://github.com/ROBOTIS-GIT/dynamixel_hardware_interface
+
+Servo IDs
+ttyUSB0, 1000000 bps, Protocol 2.0, ID: 1 Found
+ttyUSB0, 1000000 bps, Protocol 2.0, ID: 2 Found
+
---
+++ src/crab_control/crab_control/servo_controller.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+from dynamixel_sdk_custom_interfaces.msg import SetPosition
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('servo_controller')
+        self.publisher_ = self.create_publisher(SetPosition, 'set_position', 10)
+        timer_period = 0.001  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = SetPosition()
+        msg.id = 1
+        msg.position = self.i
+        self.publisher_.publish(msg)
+        self.i += 1
+        if self.i >= 4096:
+            self.i = 0
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
