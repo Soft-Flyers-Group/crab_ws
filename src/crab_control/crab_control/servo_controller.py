aeba270857f5c915a04a39096f667a2adf731a27
vm-fll <vanshmookim@gmail.com>
2026-07-08 13:11:48 -0400

    initial testing sinusoidal

--- src/crab_control/crab_control/servo_controller.py
+++ src/crab_control/crab_control/servo_controller.py
@@ -1,37 +1,55 @@
 import rclpy
 from rclpy.node import Node
 from dynamixel_sdk_custom_interfaces.msg import SetPosition
+import math
+import time
+
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('servo_controller')
-        self.publisher_ = self.create_publisher(SetPosition, 'servo/set_position', 10)
-        timer_period = 0.05  # seconds
+        self.publisher_ = self.create_publisher(SetPosition, 'set_position', 10)
+        timer_period = 0.001  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
+
+        frequency = 0.1
+        amplitude = 180.0
+
+        counts_per_degree = 4096 / 360
+        center = 2048
+
+        t = time.time() - self.start_time
+
+        angle = amplitude * math.sin(2 * math.pi * frequency * t)
+
+        position = center + angle * counts_per_degree
+
+        position = int(max(0, min(4095, position)))
+
         msg = SetPosition()
         msg.id = 1
-        msg.position = 2048
+        msg.position = self.i
         self.publisher_.publish(msg)
-        msg.id = 2
-        self.publisher_.publish(msg)
-        
+        self.i += 1
+        if self.i >= 4096:
+            self.i = 0
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    node = MinimalPublisher()
 
-    rclpy.spin(minimal_publisher)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 
