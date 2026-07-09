1bb0b923153a4ab4a005ebe21e5decb09c6f4b7a
vm-fll <vanshmookim@gmail.com>
2026-07-09 15:48:59 -0400

    first gait vansh

--- src/crab_control/crab_control/servo_controller.py
+++ src/crab_control/crab_control/servo_controller.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from dynamixel_sdk_custom_interfaces.msg import SetPosition
+from std_msgs.msg import Int32MultiArray
 import math
 import time
 
@@ -9,15 +10,50 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('servo_controller')
-        self.publisher_ = self.create_publisher(SetPosition, 'set_position', 10)
-        timer_period = 0.001  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+
+        self.publisher_ = self.create_publisher(
+            SetPosition,
+            'servo/set_position',
+            10
+        )
+
+        self.position_sub = self.create_subscription(
+        Int32MultiArray,
+        'servo/get_position',
+        self.position_callback,
+        10
+        )
+
+        self.current_positions = [0, 0]
+
+        self.timer = self.create_timer(0.001, self.timer_callback)
+
+        self.start_time = time.time()
+
+        self.position = 2048
+        self.position2 = 2048
+        self.counter = 0
+
+    def position_callback(self, msg):
+        self.current_positions = list(msg.data)
+
+        # Encoder value of servo 1
+        encoder1 = self.current_positions[0]
+
+        # Encoder value of servo 2
+        encoder2 = self.current_positions[1]
+
+        self.get_logger().info(
+            f"Servo1: {encoder1}, Servo2: {encoder2}"
+    )
+        
+    
+
 
     def timer_callback(self):
 
-        frequency = 0.1
-        amplitude = 180.0
+        frequency = 0.4
+        amplitude = 60.0
 
         counts_per_degree = 4096 / 360
         center = 2048
@@ -26,17 +62,34 @@ class MinimalPublisher(Node):
 
         angle = amplitude * math.sin(2 * math.pi * frequency * t)
 
-        position = center + angle * counts_per_degree
-
-        position = int(max(0, min(4095, position)))
+        position = int(center + angle * counts_per_degree)
+        position = max(0, min(4095, position))
 
         msg = SetPosition()
         msg.id = 1
-        msg.position = self.i
+        msg.position = position
+
+        enc = self.current_positions[0]
+
+        
+
+        if enc > 2648:
+            self.counter = 1
+
+        if self.counter == 1:
+            self.position2 = 3048
+
+        if enc < 1448:
+            self.counter = 0
+            self.position2 = 2048
+
+        msg2 = SetPosition()
+        msg2.id = 2
+        msg2.position = self.position2
+
         self.publisher_.publish(msg)
-        self.i += 1
-        if self.i >= 4096:
-            self.i = 0
+        self.publisher_.publish(msg2)
+
 
 
 def main(args=None):
