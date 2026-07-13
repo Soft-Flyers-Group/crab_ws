b7e6c892543982a718a454565564f20acb255c5d
vm-fll <vanshmookim@gmail.com>
2026-07-13 10:23:25 -0400

    gait 1

--- src/crab_control/crab_control/servo_controller.py
+++ src/crab_control/crab_control/servo_controller.py
@@ -31,6 +31,9 @@ class MinimalPublisher(Node):
         # Time tracker
         self.start_time = time.time()
 
+        self.position = 2048.0
+        self.position2 = 2048
+        self.counter = 0
         # Initial Servo positions
         self.servo_1_init = 1990
         self.servo_2_init = 1950
@@ -56,112 +59,97 @@ class MinimalPublisher(Node):
 
     def timer_callback(self):
 
-        # Defining servo messages and IDs
-        msg_1 = SetPosition()
-        msg_2 = SetPosition()
-        msg_3 = SetPosition()
-        msg_4 = SetPosition()
+        # -----------------------------
+        # Motion parameters
+        # -----------------------------
+        frequency = 0.5          # Hz
 
-        msg_1.id = 1
-        msg_2.id = 2
-        msg_3.id = 3
-        msg_4.id = 4
+        roll_amplitude = 75      # degrees
+        yaw_power = 85           # degrees during power stroke
+        yaw_recovery = 5         # degrees during recovery
 
-        msg_1.position = self.servo_1_position
-        msg_2.position = self.servo_2_position
-        msg_3.position = self.servo_3_position
-        msg_4.position = self.servo_4_position
-        
-        self.publisher_.publish(msg_1)
-        self.publisher_.publish(msg_2)
-        self.publisher_.publish(msg_3)
-        self.publisher_.publish(msg_4)
-        
-        # Old servo movement commands
-        # self.servo_3_position = self.linear_servo_move(self.servo_3_position, 1500, 0)
-        # self.servo_4_position = self.linear_servo_move(self.servo_4_position, 1000, 900)
-        
-        # self.servo_2_position = self.sin_servo_move(self.servo_2_position, 700, math.pi, 2048)
-        # self.servo_1_position = self.fourier_servo_move(self.servo_1_position, 500, math.pi, 1200)
-        
-        # self.servo_1_position = self.fourier_servo_move(self.servo_1_position, 500, math.pi, 2048)
+        power_fraction = 0.7    # portion of cycle spent pushing
 
-        # CURRENT SERVO MOVEMENT CODE
-        FLAP_RANGE = 500
-        self.servo_1_position = self.nestedsin_servo_move(self.servo_1_position, FLAP_RANGE, 0.7, self.servo_1_init, 1, 3)
-        self.servo_3_position = self.slowfast_servo_move(self.servo_3_position, FLAP_RANGE, math.pi, self.servo_3_init, -1)
+        counts_per_degree = 4096 / 360
 
-        # self.servo_2_position = self.sin_servo_move(self.servo_2_position, 500, math.pi/2, self.servo_2_init - 500)
-        # self.servo_4_position = self.sin_servo_move(self.servo_4_position, 500, math.pi/2, self.servo_4_init + 500, -1)
+        roll_center = 2048
+        yaw_center = 1650
+
+        # Time since startup
+        t = time.time() - self.start_time
+
+        # Normalized cycle position (0 to 1)
+        period = 1 / frequency
+        u = (t % period) / period
 
 
-        if self.latest_positions[0] > self.servo_1_init + FLAP_RANGE - 150:
-            self.servo_2_position = self.servo_2_init - 1000
-        if self.latest_positions[0] < self.servo_1_init - FLAP_RANGE + 150:
-            self.servo_2_position = self.servo_2_init
+        # -----------------------------
+        # ROLL (Servo 1)
+        # Sea turtle style:
+        # slow power stroke
+        # fast recovery stroke
+        # -----------------------------
+        if u < power_fraction:
 
-        # if self.latest_positions[2] > self.servo_3_init + FLAP_RANGE - 150:
-        #     self.servo_4_position = self.servo_4_init
-        # if self.latest_positions[2] < self.servo_3_init - FLAP_RANGE + 150:
-        #     self.servo_4_position = self.servo_4_init + 1000
-        
-        # self.get_logger().info('Publishing: "%s"' % str(self.latest_positions))
+            # Power stroke:
+            # -amplitude -> +amplitude
+            s = u / power_fraction
 
-    # using the sum of sin functions to move back fast and forward slowly
-    def nestedsin_servo_move(self, servo_position=0, amp=1000, omega=0.6, offset=2048, direction=1, speed=1):
-        elapsed_time = (time.time() - self.start_time) * speed
-        servo_position = round(direction * amp * math.sin(elapsed_time + omega * math.sin(elapsed_time))) + offset
-        return servo_position
-    
-    # using the sum of sin functions to move back fast and forward slowly
-    def slowfast_servo_move(self, servo_position=0, amp=1000, omega=math.pi, offset=2048, direction=1):
-        elapsed_time = time.time() - self.start_time
+            roll_angle = (
+                -roll_amplitude * math.cos(math.pi * s)
+            )
 
-        n = np.arange(1, 100)
-        sin_series = amp * np.sin(n * elapsed_time * omega) / n
-        servo_position = round(np.sum(sin_series)/5) * 5 * direction + offset
-
-        return servo_position
-
-    # using Fourier series to define servo movement emulating piecewise on-off
-    def fourier_servo_move(self, servo_position=0,amp=1000, omega=math.pi, offset=2048, direction=1):
-
-        elapsed_time = time.time() - self.start_time
-
-        n = np.arange(1, 300) # creates array [1, 2, ... a]
-        signs = (-1) ** (n + 1)
-
-        # calculates terms of fourier series
-        fourier_series = signs * 4 / math.pi * amp * np.cos((2*n - 1) * omega * elapsed_time) / (2 * n + 1)
-        servo_position = round(np.sum(fourier_series)/5) * 5 * direction + offset
-
-        return servo_position
-
-    # using a sinusoidal function to define back and forth servo movement
-    def sin_servo_move(self, servo_position=0, amp=1000, omega=math.pi, offset=2048, direction=1):
-        
-        elapsed_time = time.time() - self.start_time
-
-        servo_position = round(direction * amp * math.cos(omega * elapsed_time) + offset)
-
-        return servo_position
-    
-    # using a linear addition to define back and forth servo momement
-    def linear_servo_movem(self, servo_position=0, steps=1000, start=0):
-
-        if servo_position >= steps + start:
-            self.increasing = False
-        elif servo_position <= start:
-            self.increasing = True
-        
-        if self.increasing:
-            servo_position += 3
         else:
-            servo_position -= 5
-        
-        return servo_position
 
-# publishing messages to the servos
+            # Recovery stroke:
+            # +amplitude -> -amplitude
+            s = (u - power_fraction) / (1 - power_fraction)
+
+            roll_angle = (
+                roll_amplitude * math.cos(math.pi * s)
+            )
+
+
+        if u < power_fraction:
+            yaw_angle = yaw_power
+        else:
+            yaw_angle = yaw_recovery
+
+        # Convert degrees to encoder counts
+        roll_position = int(
+            roll_center +
+            roll_angle * counts_per_degree
+        )
+
+        yaw_position = int(
+            yaw_center +
+            yaw_angle * counts_per_degree
+        )
+
+
+        # Keep positions safe
+        roll_position = max(0, min(4095, roll_position))
+        yaw_position = max(0, min(4095, yaw_position))
+
+
+        # -----------------------------
+        # Send commands
+        # -----------------------------
+        msg1 = SetPosition()
+        msg1.id = 1
+        msg1.position = roll_position
+
+
+        msg2 = SetPosition()
+        msg2.id = 2
+        msg2.position = yaw_position
+
+
+        self.publisher_.publish(msg1)
+        self.publisher_.publish(msg2)
+
+
+
 def main(args=None):
     rclpy.init(args=args)
 
---
+++ src/crab_control/crab_control/servo_node_sync.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Wonho Yun, Will Son
+
+
+from dynamixel_sdk import COMM_SUCCESS
+from dynamixel_sdk import PacketHandler
+from dynamixel_sdk import PortHandler
+from dynamixel_sdk_custom_interfaces.msg import SetPosition
+from dynamixel_sdk import *
+from std_msgs.msg import Int32MultiArray
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+
+# Control table address for 
+ADDR_OPERATING_MODE = 11 # Control table address is different in Dynamixel model
+ADDR_TORQUE_ENABLE = 64
+ADDR_GOAL_POSITION = 116
+ADDR_PRESENT_POSITION = 132
+ADDR_HOMING_OFFSET = 20
+
+# Protocol version
+PROTOCOL_VERSION = 2.0  # Default Protocol version of DYNAMIXEL X series.
+
+# Default settings
+BAUDRATE = 1000000  # Dynamixel default baudrate : 57600
+DEVICE_NAME = '/dev/ttyUSB0'  # Check which port is being used on your controller
+
+TORQUE_ENABLE = 1  # Value for enabling the torque
+TORQUE_DISABLE = 0  # Value for disabling the torque
+# 4096 pulses per revolution
+POSITION_CONTROL = 3  # Value 4 for Extended position control mode, 3 for normal position control
+DATA_LENGTH_4BYTE = 4
+
+# Amount of servos to init on the bus (MAX 4)
+NUM_SERVOS = 2
+HOMING_OFFSET = -1000
+
+
+class ReadWriteNode(Node):
+    def __init__(self):
+        super().__init__('read_write_node')
+
+        # List of servo goals and positions
+        self.servo_goals = [0,0]
+        self.servo_positions = [0,0]
+
+        
+        self.port_handler = PortHandler(DEVICE_NAME)
+        self.packet_handler = PacketHandler(PROTOCOL_VERSION)
+
+        if not self.port_handler.openPort():
+            self.get_logger().error('Failed to open the port!')
+            return
+        self.get_logger().info('Succeeded to open the port.')
+
+        if not self.port_handler.setBaudRate(BAUDRATE):
+            self.get_logger().error('Failed to set the baudrate!')
+            return
+        self.get_logger().info('Succeeded to set the baudrate.')
+
+        # Create group sync read and write objects
+        self.groupSyncWrite = GroupSyncWrite(self.port_handler, self.packet_handler, ADDR_GOAL_POSITION, DATA_LENGTH_4BYTE)
+        self.groupSyncRead = GroupSyncRead(self.port_handler, self.packet_handler, ADDR_PRESENT_POSITION, DATA_LENGTH_4BYTE)
+
+        # Setup N Servos (4 Max for this project)
+        for i in range(1, NUM_SERVOS+1):
+            self.setup_dynamixel(i)
+        
+        for dxl_id in range(1, NUM_SERVOS + 1):
+            if not self.groupSyncRead.addParam(dxl_id):
+                self.get_logger().error(f"Failed to add servo {dxl_id} to GroupSyncRead")
+
+        # Setup the subscriber for servo goals along with the service to get feedback
+        qos = QoSProfile(depth=10)
+        self.subscription = self.create_subscription(
+            SetPosition,
+            f'servo/set_position',
+            self.set_position_callback,
+            qos
+        )
+        self.publisher = self.create_publisher(Int32MultiArray, f'servo/get_position', qos)
+        self.timer = self.create_timer(0.25, self.get_position_callback)
+
+    def setup_dynamixel(self, dxl_id):
+        dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+            self.port_handler, dxl_id, ADDR_OPERATING_MODE, POSITION_CONTROL
+        )
+        if dxl_comm_result != COMM_SUCCESS:
+            self.get_logger().error(f'Failed to set Position Control Mode: \
+                                    {self.packet_handler.getTxRxResult(dxl_comm_result)}')
+        else:
+            self.get_logger().info('Succeeded to set Position Control Mode.')
+
+
+        dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
+            self.port_handler, dxl_id, ADDR_HOMING_OFFSET, HOMING_OFFSET
+        )
+        if dxl_comm_result != COMM_SUCCESS:
+            self.get_logger().error(f'Failed to set Homing Offset: \
+                                    {self.packet_handler.getTxRxResult(dxl_comm_result)}')
+        else:
+            self.get_logger().info('Succeeded to set Homing Offset.')
+
+
+        dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+            self.port_handler, dxl_id, ADDR_TORQUE_ENABLE, TORQUE_ENABLE
+        )
+        if dxl_comm_result != COMM_SUCCESS:
+            self.get_logger().error(f'Failed to enable torque: \
+                                    {self.packet_handler.getTxRxResult(dxl_comm_result)}')
+        else:
+            self.get_logger().info('Succeeded to enable torque.')
+
+    def group_sync_write(self, goals):
+        # Package goals
+        for idx, goal in enumerate(goals):
+                param_goal_position = [
+                    DXL_LOBYTE(DXL_LOWORD(goal)),
+                    DXL_HIBYTE(DXL_LOWORD(goal)),
+                    DXL_LOBYTE(DXL_HIWORD(goal)),
+                    DXL_HIBYTE(DXL_HIWORD(goal))
+                ]
+                dxl_addparam_result = self.groupSyncWrite.addParam(idx+1, param_goal_position)
+                if not dxl_addparam_result:
+                    self.get_logger().error("[ID:%03d] groupSyncWrite addparam failed" % (idx+1))
+        # Send Goals
+        dxl_comm_result = self.groupSyncWrite.txPacket()
+        if dxl_comm_result != COMM_SUCCESS:
+            self.get_logger().error("%s" % self.packet_handler.getTxRxResult(dxl_comm_result))
+        self.groupSyncWrite.clearParam()
+    
+    def set_position_callback(self, msg):
+        # Update goal array
+        self.servo_goals[msg.id-1] = msg.position
+        # Send out the array
+        self.group_sync_write(self.servo_goals)
+    
+
+    def get_position_callback(self):
+        
+        msg = Int32MultiArray()
+        dxl_comm_result = self.groupSyncRead.txRxPacket()
+        if dxl_comm_result != COMM_SUCCESS:
+            self.get_logger().error("%s" % self.packet_handler.getTxRxResult(dxl_comm_result))
+        
+        for idx, position in enumerate(self.servo_positions):
+            self.servo_positions[idx] = self.groupSyncRead.getData(idx+1, ADDR_PRESENT_POSITION, DATA_LENGTH_4BYTE)
+        
+        msg.data = self.servo_positions
+        self.publisher.publish(msg)
+
+
+    def __del__(self):
+        self.packet_handler.write1ByteTxRx(self.port_handler,
+                                           1,
+                                           ADDR_TORQUE_ENABLE,
+                                           TORQUE_DISABLE)
+        self.port_handler.closePort()
+        self.get_logger().info('Shutting down read_write_node')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ReadWriteNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
